a43cd87edef9ba4788bdb848784046661311e671
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2024-04-24 17:45:35 +0200

    Actividad 4

---
+++ UD6 - NHibernate/Actividades/Actividad4/ActividadNHibernate/Entidades/Carrera.cs
@@ -0,0 +1,22 @@
+﻿using NHibernate.Mapping.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadNHibernate.Entidades
+{
+    [Serializable]
+    [Class(Schema = "dbo", Table = "Carrera", NameType = typeof(Carrera))]
+    public class Carrera
+    {
+        [Id(Name = "IdCarrera", Column = "IdCarrera", Type = "int"), Generator(1, Class = "identity")]
+        public virtual int IdCarrera { get; set; }
+        [Property(Column = "NombreCarrera", Type = "string", NotNull = true)]
+        public virtual String NombreCarrera { get; set; }
+
+        [ManyToOne(ClassType = typeof(Instituto), OuterJoin = OuterJoinStrategy.True)]
+        public virtual Instituto UnInstituto { get; set; }
+    }
+}
---
+++ UD6 - NHibernate/Actividades/Actividad4/ActividadNHibernate/Entidades/Estudiante.cs
@@ -0,0 +1,35 @@
+﻿using NHibernate.Mapping;
+using NHibernate.Mapping.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadNHibernate.Entidades
+{
+    [Serializable]
+    [Class(Schema = "dbo", Table = "Estudiante", NameType = typeof(Estudiante))]
+    public class Estudiante
+    {
+        [Id(Name = "IdEstudiante", Column = "IdEstudiante", Type = "int"), Generator(1, Class = "identity")]
+        public virtual int IdEstudiante { get; set; }
+
+        [Property(Column = "NombreEstudiante", Type = "string", NotNull = true)]
+        public virtual String NombreEstudiante { get; set; }
+
+        [Property(Column = "ApellidoEstudiante", Type = "string", NotNull = true)]
+        public virtual String ApellidoEstudiante { get; set; }
+
+        [Property(Column = "DocumentoEstudiante", Type = "string", NotNull = true)]
+        public virtual String DocumentoEstudiante { get; set; }
+
+        [ManyToOne(ClassType = typeof(Instituto), OuterJoin = OuterJoinStrategy.True)]
+        public virtual Instituto UnInstituto { get; set; }
+
+        [Set(0, Name = "ListaAsignaturas", Table = "estudiante_asignatura", Inverse = false, Cascade = "all")]
+        [Key(1, Column = "IdAsignatura")]
+        [ManyToMany(2, ClassType = typeof(Asignatura), Column = "IdEstudiante")]
+        public virtual List<Asignatura> ListaAsignaturas { get; set; }
+    }
+}
